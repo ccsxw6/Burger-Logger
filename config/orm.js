2337d601609871c3a091bc6fb17956b0837b60a3
ccsxw6 <ccsxw6@gmail.com>
2021-02-15 13:38:32 -0600

    updated orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,12 +1,22 @@
-// Import MySQL connection.
-const connection = require('./connection.js');
+var connection = require("./connection.js");
 
+
+// Helper function for SQL syntax.
+// Let's say we want to pass 3 values into the mySQL query.
+// In order to write the query, we need 3 question marks.
+// Loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// ["?", "?", "?"].toString() => "?,?,?";
+const printQuestionMarks = (num) => {
+    const arr = [];
+    for (let i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+};
 
 // Helper function to convert object key/value pairs to SQL syntax
-// TOOK THIS FROM CAT.JS ACTIVITY 16
 const objToSql = (ob) => {
   const arr = [];
-
   // loop through the keys and push the key/value as a string int arr
   for (const key in ob) {
     let value = ob[key];
@@ -21,53 +31,59 @@ const objToSql = (ob) => {
       arr.push(`${key}=${value}`);
     }
   }
-
   // translate array of strings to a single comma-separated string
   return arr.toString();
 };
 
+var orm = {
+    selectAll(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    // vals is an array of values that we want to save to cols
+    // cols are the columns we want to insert the values into
+    insertOne(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
 
-const orm = {
-  selectAll(table_name, cb) {
-    const queryString = 'SELECT * FROM ??';
-    connection.query(
-      queryString, [table_name],
-      (err, result) => {
-        if (err) {
-          throw err;
-        }
-        cb(result)
-        console.log(result);
-      });
-  },
-  insertOne(table_Name, column1, column2, burer, TorF) {
-    const queryString = 'INSERT INTO ?? (??, ??) VALUES (?, ?)';
-    connection.query(
-      queryString, [table_Name, column1, column2, burer, TorF], 
-      (err, result) => {
-        if (err) throw err;
-        console.log(result)
-      })
-  },
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
 
-//STOPPED HERE - NOT SURE HOW TO MAKE THIS JUAN WORK
+        console.log(queryString);
 
-  updateOne(table_name, objColVals, condition, cb) {
-    let queryString = `UPDATE ${table_name}`;
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    // objColVals would be the columns and values that you want to update
+    // an example of objColVals would be {name: panther, sleepy: true}
+    updateOne(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
 
-    queryString += ' SET ';
-    queryString += objToSql(objColVals);
-    queryString += ' WHERE ';
-    queryString += condition;
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
 
-    console.log(queryString);
-    connection.query(queryString, (err, result) => {
-      if (err) {
-        throw err;
-      }
-      cb(result);
-    });
-  },
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
 };
 
-  module.exports = orm;
+module.exports = orm;
